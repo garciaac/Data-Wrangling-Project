83f7a20949359c153762721cd2a25136e30b7e86
Andrew Garcia <andrew@Andrews-MacBook-Pro.local>
2015-12-15 11:53:50 -0800

    Finished R script

--- run_analysis.R
+++ run_analysis.R
@@ -15,16 +15,31 @@ target_data <- tbl_df(target_data)
 train_labels <- scan("train/y_train.txt")
 test_labels <- scan("test/y_test.txt")
 combined_labels <- c(train_labels, test_labels)
-target_data$ActivityLabel <- combined_labels
+target_data$activityLabel <- combined_labels
 
 activity_labels <- read.table("activity_labels.txt")[[2]]
-target_data$ActivityName <- activity_labels[target_data$ActivityLabel]
+target_data$activityName <- activity_labels[target_data$activityLabel]
+
+train_subjects <- scan("train/subject_train.txt")
+test_subjects <- scan("test/subject_test.txt")
+combined_subjects <- c(train_subjects, test_subjects)
+target_data$subject <- combined_subjects
 
 target_data$id <- c(1:nrow(target_data))
 target_data <- target_data %>%
-	gather(measure, value, -id, -ActivityLabel, -ActivityName)
+	gather(measure, value, -id, -activityLabel, -activityName, -subject)
 
 target_data$axis <- ifelse(grepl("...X", target_data$measure), "x", ifelse(grepl("...Y", target_data$measure), "y", "z"))
 target_data$stat <- ifelse(grepl("[a-zA-Z]*.mean", target_data$measure), "mean", "std")
 target_data$measure <- substr(target_data$measure, 1, regexpr("\\.", target_data$measure)-1)
 
+values_by_subject <- target_data %>%
+	group_by(subject, measure, axis) %>%
+		summarise(mean(value))
+
+values_by_activity <- target_data %>%
+	group_by(activityName, measure, axis) %>%
+		summarise(mean(value))
+
+cat(head(values_by_subject))
+cat(head(values_by_activity))
